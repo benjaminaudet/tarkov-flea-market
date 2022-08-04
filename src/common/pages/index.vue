df045c603cc1bbc1790ad340d49ee5e1ded064f2
benjaminaudet <benjamin@audet.fr>
2022-08-05 01:29:01 +0200

    Add infinite scroll and search

--- src/common/pages/index.vue
+++ src/common/pages/index.vue
@@ -9,13 +9,14 @@ const { result, loading, error } = useGetItemsQuery();
 
 const pageIndex = ref(20);
 const search = ref("");
+const data = ref([]);
 
 function onScroll(state: UseScrollReturn) {
-  console.log(state); // {x, y, isScrolling, arrivedState, directions}
+  if (state.arrivedState.bottom) {
+    increasePageIndex();
+  }
 }
 
-const { results } = useFuse(search, result);
-console.log(results);
 // Routing
 const router = useRouter();
 const go = () => {
@@ -29,15 +30,29 @@ const timestamp = ref(1183135260000);
 function increasePageIndex() {
   pageIndex.value += 20;
 }
+
+watch(search, () => {
+  if (search.value === "") {
+    data.value = result.value.items;
+  }
+  data.value = result.value.items.filter((a) => a.name.includes(search.value));
+});
+
+watch(result, () => {
+  if (result.value) {
+    data.value = result.value.items
+    console.log(data.value);
+  }
+});
 </script>
 
 <template>
   <div>
-    <div v-scroll="onScroll">
+    <div>
       <div v-if="loading">
         <div>
           <n-space vertical>
-            <n-input v-model:value="search" type="text" placeholder="Search" />
+            <n-input type="text" placeholder="Search" />
             <n-grid x-gap="12" y-gap="12" :cols="5">
               <n-gi v-for="i in new Array(20)">
                 <VItemCard :loading="loading" />
@@ -48,14 +63,18 @@ function increasePageIndex() {
         </div>
       </div>
       <div v-else-if="error">Error: {{ error.message }}</div>
-      <div v-else-if="result">
+      <div>
         <n-space vertical>
-          <n-input v-model:value="value" type="text" placeholder="Search" />
-          <n-grid x-gap="12" y-gap="12" :cols="5">
-            <n-gi
-              v-for="item in result?.items.slice(0, pageIndex)"
-              :key="item.id"
-            >
+          <n-input v-model:value="search" type="text" placeholder="Search" />
+          <n-grid
+            v-if="data"
+            x-gap="12"
+            y-gap="12"
+            :cols="5"
+            class="overflow-y-auto h-[47rem]"
+            v-scroll="onScroll"
+          >
+            <n-gi v-for="item in data?.slice(0, pageIndex)" :key="item.id">
               <VItemCard :loading="loading" :item="item" />
             </n-gi>
           </n-grid>

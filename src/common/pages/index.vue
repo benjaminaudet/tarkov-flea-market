e17f3123be9c36cc48a20df1bdd9a268940e6309
benjaminaudet <benjamin@audet.fr>
2022-08-04 23:51:42 +0200

    test

--- src/common/pages/index.vue
+++ src/common/pages/index.vue
@@ -1,69 +1,72 @@
 <script setup lang="ts">
 // graphql example
-import { useGetItemsQuery } from '~/common/services/useExample.query'
-import { useFuse } from '@vueuse/integrations/useFuse'
-import { useScroll } from '@vueuse/core'
+import { useGetItemsQuery } from "~/common/services/useExample.query";
+import { useFuse } from "@vueuse/integrations/useFuse";
+import type { UseScrollReturn } from "@vueuse/core";
+import { vScroll } from "@vueuse/components";
 
-const { result, loading, error } = useGetItemsQuery()
+const { result, loading, error } = useGetItemsQuery();
 
+const pageIndex = ref(20);
+const search = ref("");
 
-const pageIndex = ref(20)
-const search = ref('')
-const el = ref<HTMLElement | null>(null)
-const { arrivedState } = useScroll(el)
-const { bottom } = toRefs(arrivedState)
-
-const { results } = useFuse(search, result)
-console.log(results)
-// Routing
-const router = useRouter()
-const go = () => {
-  router.push('/users/home')
+function onScroll(state: UseScrollReturn) {
+  console.log(state); // {x, y, isScrolling, arrivedState, directions}
 }
 
+const { results } = useFuse(search, result);
+console.log(results);
+// Routing
+const router = useRouter();
+const go = () => {
+  router.push("/users/home");
+};
+
 watch(bottom, (bottom: boolean) => {
-  console.log(bottom)
-  increasePageIndex()
-})
+  console.log(bottom);
+  increasePageIndex();
+});
 
 // Internationalization
-const { t } = useI18n()
-const timestamp = ref(1183135260000)
+const { t } = useI18n();
+const timestamp = ref(1183135260000);
 
 function increasePageIndex() {
   pageIndex.value += 20;
 }
-
 </script>
 
 <template>
   <div>
-    <div v-if="loading">
-    <div>
-      <n-space vertical>
-        <n-input v-model:value="search" type="text" placeholder="Search" />
-        <n-grid x-gap="12" y-gap="12" :cols="5">
-          <n-gi v-for="i in new Array(20)">
-            <VItemCard :loading="loading" />
-          </n-gi>
-        </n-grid>
-        <VButton @click="increasePageIndex">More...</VButton>
-      </n-space>
-    </div>
-    </div>
-    <div v-else-if="error">
-      Error: {{ error.message }}
-    </div>
-    <div v-else-if="result">
-      <n-space vertical>
-        <n-input v-model:value="value" type="text" placeholder="Search" />
-        <n-grid x-gap="12" y-gap="12" :cols="5">
-          <n-gi v-for="item in result?.items.slice(0, pageIndex)" :key="item.id">
-            <VItemCard :loading="loading" :item="item" />
-          </n-gi>
-        </n-grid>
-        <VButton @click="increasePageIndex">More...</VButton>
-      </n-space>
+    <div v-scroll="onScroll">
+      <div v-if="loading">
+        <div>
+          <n-space vertical>
+            <n-input v-model:value="search" type="text" placeholder="Search" />
+            <n-grid x-gap="12" y-gap="12" :cols="5">
+              <n-gi v-for="i in new Array(20)">
+                <VItemCard :loading="loading" />
+              </n-gi>
+            </n-grid>
+            <VButton @click="increasePageIndex">More...</VButton>
+          </n-space>
+        </div>
+      </div>
+      <div v-else-if="error">Error: {{ error.message }}</div>
+      <div v-else-if="result">
+        <n-space vertical>
+          <n-input v-model:value="value" type="text" placeholder="Search" />
+          <n-grid x-gap="12" y-gap="12" :cols="5">
+            <n-gi
+              v-for="item in result?.items.slice(0, pageIndex)"
+              :key="item.id"
+            >
+              <VItemCard :loading="loading" :item="item" />
+            </n-gi>
+          </n-grid>
+          <VButton @click="increasePageIndex">More...</VButton>
+        </n-space>
+      </div>
     </div>
   </div>
 </template>

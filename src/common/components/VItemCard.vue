cc87bb3f68a32f1a2e08dfd7a8558cfb71b93fe6
benjaminaudet <benjamin@audet.fr>
2022-08-08 18:00:18 +0200

    Add quests info + WIP icon

--- src/common/components/VItemCard.vue
+++ src/common/components/VItemCard.vue
@@ -3,6 +3,8 @@ import { computed } from 'vue'
 import { useElementVisibility } from '@vueuse/core'
 import _ from 'lodash'
 
+import IconWIP from '~icons/wpf/maintenance'
+
 const props = defineProps({
   item: Object,
   loading: Boolean,
@@ -25,10 +27,18 @@ if (props && props.item) {
         <n-skeleton text />
       </template>
       <n-tabs type="line" size="small" :tabs-padding="20" pane-style="padding: 20px;">
-        <n-tab-pane name="Price Stats">
+        <n-tab-pane name="Price Info">
+          <n-skeleton text />
           <n-skeleton text />
         </n-tab-pane>
-        <n-tab-pane name="General Stats">
+        <n-tab-pane name="General Info">
+          <n-skeleton text />
+          <n-skeleton text />
+          <n-skeleton text />
+        </n-tab-pane>
+        <n-tab-pane name="Quests Info">
+          <n-skeleton text />
+          <n-skeleton text />
           <n-skeleton text />
         </n-tab-pane>
       </n-tabs>
@@ -44,7 +54,7 @@ if (props && props.item) {
         </div>
       </template>
       <n-tabs type="segment" size="small" :tabs-padding="20" pane-style="padding: 20px;">
-        <n-tab-pane name="Flea Stats">
+        <n-tab-pane name="Flea Info">
           <n-grid x-gap="12" y-gap="12" cols="1">
             <n-gi class="text-left">
               <n-tag :bordered="false" class="rounded-r-none w-[50%]">
@@ -73,7 +83,7 @@ if (props && props.item) {
             <a :href="item.wikiLink" target="_blank">Page Wiki</a>
           </VButton>
         </n-tab-pane>
-        <n-tab-pane name="Traders Stats">
+        <n-tab-pane name="Traders Info">
           <n-grid x-gap="12" y-gap="12" cols="1">
             <n-gi v-for="trader in sellFor" :key="`${item.id}:${trader?.vendor?.name}`" class="text-left">
               <n-tag :bordered="false" class="rounded-r-none w-[50%]">
@@ -88,6 +98,11 @@ if (props && props.item) {
               </n-tag>
             </n-gi>
           </n-grid>
+        </n-tab-pane>
+        <n-tab-pane name="Quests Info">
+          <IconWIP style="display:inline" />
+          Work in progress
+          <IconWIP style="display:inline" />
         </n-tab-pane>
       </n-tabs>
     </n-card>

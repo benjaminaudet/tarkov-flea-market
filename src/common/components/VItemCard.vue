ad011e1e3b8dd2866d8757675e711bc119121d86
benjaminaudet <benjamin@audet.fr>
2022-08-09 17:02:44 +0200

    Add buying filter

--- src/common/components/VItemCard.vue
+++ src/common/components/VItemCard.vue
@@ -3,6 +3,7 @@ import { computed } from 'vue'
 import { useElementVisibility } from '@vueuse/core'
 import _ from 'lodash'
 
+import colors from 'tailwindcss/colors'
 import IconWIP from '~icons/wpf/maintenance'
 
 const props = defineProps({
@@ -10,12 +11,30 @@ const props = defineProps({
   loading: Boolean,
 })
 
+const activeTab = ref('flea')
 const sellFor = ref([])
+const buyFor = ref([])
 
 if (props && props.item) {
   sellFor.value = [...props.item.sellFor]
+  buyFor.value = [...props.item.buyFor]
+
   sellFor.value.pop()
+  buyFor.value.pop()
   sellFor.value = _.orderBy(sellFor.value, 'priceRUB', 'desc')
+  buyFor.value = _.orderBy(buyFor.value, 'priceRUB', 'desc')
+}
+
+const getSellForObjectByTrader = (trader) => {
+  return sellFor.value.find(el => el.vendor.name === trader)
+}
+
+const getBuyForObjectByTrader = (trader) => {
+  return props.item.buyFor.find(el => el.vendor.name === trader)
+}
+
+const handleChange = (tab) => {
+  activeTab.value = tab
 }
 </script>
 
@@ -26,7 +45,7 @@ if (props && props.item) {
         <n-skeleton height="50px" width="50px" />
         <n-skeleton text />
       </template>
-      <n-tabs type="line" size="small" :tabs-padding="20" pane-style="padding: 20px;">
+      <n-tabs type="card" size="large" :tabs-padding="20" pane-style="padding: 20px;">
         <n-tab-pane name="Price Info">
           <n-skeleton text />
           <n-skeleton text />
@@ -53,58 +72,74 @@ if (props && props.item) {
           {{ item.name }}
         </div>
       </template>
-      <n-tabs type="segment" size="small" :tabs-padding="20" pane-style="padding: 20px;">
-        <n-tab-pane name="Flea Info">
-          <n-grid x-gap="12" y-gap="12" cols="1">
-            <n-gi class="text-left">
-              <n-tag :bordered="false" class="rounded-r-none w-[50%]">
-                Prix moyen 24h
-              </n-tag>
-              <n-tag :bordered="false" type="success" class="bold rounded-l-none w-[50%]">
-                <n-number-animation
-                  ref="numberAnimationInstRef"
-                  :duration="300" show-separator :from="0" :to="item.avg24hPrice"
-                />
-                ₽
-              </n-tag>
-            </n-gi>
-          </n-grid>
-          <n-tag :bordered="false" class="rounded-r-none  w-[50%]">
-            Variation prix 48h
-          </n-tag>
-          <n-tag :bordered="false" type="success" class="bold rounded-l-none w-[50%]">
-            <n-number-animation
-              ref="numberAnimationInstRef"
-              :duration="300" show-separator :from="0" :to="item.changeLast48h"
-            />
-            ₽
-          </n-tag>
-          <VButton>
-            <a :href="item.wikiLink" target="_blank">Page Wiki</a>
-          </VButton>
-        </n-tab-pane>
-        <n-tab-pane name="Traders Info">
-          <n-grid x-gap="12" y-gap="12" cols="1">
-            <n-gi v-for="trader in sellFor" :key="`${item.id}:${trader?.vendor?.name}`" class="text-left">
-              <n-tag :bordered="false" class="rounded-r-none w-[50%]">
-                {{ trader?.vendor?.name }}
-              </n-tag>
-              <n-tag :bordered="false" type="success" class="bold rounded-l-none w-[50%]">
-                <n-number-animation
-                  ref="numberAnimationInstRef"
-                  :duration="300" show-separator :from="0" :to="trader?.priceRUB"
-                />
-                ₽
-              </n-tag>
-            </n-gi>
-          </n-grid>
-        </n-tab-pane>
-        <n-tab-pane name="Quests Info">
-          <IconWIP style="display:inline" />
-          Work in progress
-          <IconWIP style="display:inline" />
-        </n-tab-pane>
+      <n-tabs :on-update:value="handleChange" type="card" size="large" :tabs-padding="20" class="mb-4">
+        <n-tab name="flea" class="active:text-teal-400">
+          Flea Info
+        </n-tab>
+        <n-tab name="traders">
+          Traders Info
+        </n-tab>
+        <n-tab name="quests">
+          Quests Info
+        </n-tab>
       </n-tabs>
+      <div v-if="activeTab === 'flea'">
+        <n-grid x-gap="12" y-gap="12" cols="1">
+          <n-gi class="text-left">
+            <n-tag :bordered="false" class="rounded-r-none w-[50%]">
+              Prix moyen 24h
+            </n-tag>
+            <n-tag :bordered="false" type="success" class="bold rounded-l-none w-[50%]">
+              <n-number-animation
+                ref="numberAnimationInstRef"
+                :duration="300" show-separator :from="0" :to="item.avg24hPrice"
+              />
+              ₽
+            </n-tag>
+          </n-gi>
+        </n-grid>
+        <n-tag :bordered="false" class="rounded-r-none  w-[50%]">
+          Variation prix 48h
+        </n-tag>
+        <n-tag :bordered="false" type="success" class="bold rounded-l-none w-[50%]">
+          <n-number-animation
+            ref="numberAnimationInstRef"
+            :duration="300" show-separator :from="0" :to="item.changeLast48h"
+          />
+          ₽
+        </n-tag>
+        <VButton>
+          <a :href="item.wikiLink" target="_blank">Page Wiki</a>
+        </VButton>
+      </div>
+      <div v-else-if="activeTab === 'traders'">
+        <n-grid x-gap="12" y-gap="12" cols="1">
+          <n-gi v-for="trader in ['Prapor', 'La Toubib', 'Fence', 'Skier', 'Peacekeeper', 'Mechanic', 'Ragman', 'Jaeger']" :key="`${item.id}:${trader}`">
+            <n-tag v-if="getSellForObjectByTrader(trader) || getBuyForObjectByTrader(trader)" :bordered="false" class="rounded-r-none w-[33%] text-center">
+              {{ trader }}
+            </n-tag>
+            <n-tag v-if="getSellForObjectByTrader(trader) || getBuyForObjectByTrader(trader)" :bordered="false" type="success" class="bold rounded-l-none rounded-r-none w-[33%] text-center">
+              <n-number-animation
+                ref="numberAnimationInstRef"
+                :duration="300" show-separator :from="0" :to="getSellForObjectByTrader(trader)?.priceRUB || 0"
+              />
+              ₽
+            </n-tag>
+            <n-tag v-if="getSellForObjectByTrader(trader) || getBuyForObjectByTrader(trader)" :bordered="false" type="warning" class="bold rounded-l-none w-[33%] text-center">
+              <n-number-animation
+                ref="numberAnimationInstRef"
+                :duration="300" show-separator :from="0" :to="getBuyForObjectByTrader(trader)?.price"
+              />
+              {{ getBuyForObjectByTrader(trader)?.currency }}
+            </n-tag>
+          </n-gi>
+        </n-grid>
+      </div>
+      <div v-else-if="activeTab === 'quests'">
+        <IconWIP style="display:inline" />
+        Work in progress
+        <IconWIP style="display:inline" />
+      </div>
     </n-card>
   </n-space>
 </template>
--- src/common/pages/index.vue
+++ src/common/pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import type { UseScrollReturn, refDebounced } from '@vueuse/core'
+import { refDebounced } from '@vueuse/core'
+import type { UseScrollReturn } from '@vueuse/core'
 import { vScroll } from '@vueuse/components'
 import colors from 'tailwindcss/colors'
 import { useGetItemsQuery } from '~/common/services/useGetItems.query'
@@ -7,15 +8,18 @@ import { useGetItemsQuery } from '~/common/services/useGetItems.query'
 import IconAscendingSort from '~icons/bi/sort-numeric-down'
 import IconDescendingSort from '~icons/bi/sort-numeric-up'
 
-const { result } = useGetItemsQuery()
+const { result, loading, error } = useGetItemsQuery()
 
 const pageIndex = ref(80)
 const searchInput = ref('')
 const searchInputDebounced = refDebounced(searchInput, 500) // fire finished
 const searchInputDebouncedLoading = refDebounced(searchInput, 50) // fire loading
 const data = ref([])
+const dataToUse = ref([])
 const sortDirectionAsc = ref(true)
 const sortType = ref('avg24hPrice')
+const scrollContainerRef = ref<HTMLElement | undefined>(undefined)
+const target = scrollContainerRef.value
 
 const onScroll = (state: UseScrollReturn) => {
   if (state.arrivedState.bottom)
@@ -58,49 +62,69 @@ watch(result, () => {
   }
 })
 
-
-const sortByTrader = (key: string) => {
-  console.log(key)
+const sortByTraderToBuy = (key: string) => {
   sortType.value = key
-  data.value = [...data.value]
-  data.value = data.value.filter((el) => {
-    console.log(el)
-    return el.sellFor.find((_el) => _el.vendor.name === key)
+  dataToUse.value = [...data.value]
+  dataToUse.value = dataToUse.value.filter((el) => {
+    return el.buyFor.find(_el => _el.vendor.name === key)
   })
-  data.value = [...data.value]
-  data.value = data.value.sort((a, b) => {
-    console.log(a?.sellFor?.find((_el => _el?.vendor?.name === key)))
-    const _a = a?.sellFor?.find((_el => _el?.vendor?.name === key))?.priceRUB
-    console.log(_a)
-    const _b = b?.sellFor?.find((_el => _el?.vendor?.name === key))?.priceRUB
-    sortDirectionAsc.value ? _b - _a : _a - _b
+  dataToUse.value = dataToUse.value.sort((a, b) => {
+    const _a = a?.buyFor?.find(_el => _el?.vendor?.name === key)?.priceRUB
+    const _b = b?.buyFor?.find(_el => _el?.vendor?.name === key)?.priceRUB
+    return sortDirectionAsc.value ? _b - _a : _a - _b
   })
-  console.log(data.value)
+}
+
+const sortByTraderToSell = (key: string) => {
+  sortType.value = key
+  dataToUse.value = [...data.value]
+  dataToUse.value = dataToUse.value.filter((el) => {
+    return el.sellFor.find(_el => _el.vendor.name === key)
+  })
+  dataToUse.value = dataToUse.value.sort((a, b) => {
+    const _a = a?.sellFor?.find(_el => _el?.vendor?.name === key)?.priceRUB
+    const _b = b?.sellFor?.find(_el => _el?.vendor?.name === key)?.priceRUB
+    return sortDirectionAsc.value ? _b - _a : _a - _b
+  })
 }
 
 const sort = (key: string) => {
-  console.log('sort')
   sortType.value = key
-  data.value = [...data.value]
-  data.value = data.value.sort((a, b) =>
+  dataToUse.value = [...data.value]
+  dataToUse.value = dataToUse.value.sort((a, b) =>
     sortDirectionAsc.value ? b[key] - a[key] : a[key] - b[key],
   )
 }
 
 const toggleSort = (key) => {
+  if (key === sortType.value)
+    sortDirectionAsc.value = !sortDirectionAsc.value
   if (['avg24hPrice', 'changeLast48h'].includes(key))
-    sort(key)
-  else
-    sortByTrader(key)
-  sortDirectionAsc.value = !sortDirectionAsc.value
+    sort(key.split(':')[0])
+  else if (key.includes('sell'))
+    sortByTraderToSell(key.split(':')[0])
+  else if (key.includes('buy'))
+    sortByTraderToBuy(key.split(':')[0])
 }
 
 const sortOptions = [
   { label: 'Trier par prix moyen 24h', value: 'avg24hPrice' },
   { label: 'Trier par variation du prix dernières 48h', value: 'changeLast48h' },
-  { label: 'Trier avec la Toubib pour vendre', value: "La Toubib" }
+  { label: 'Trier avec la Toubib pour vendre', value: 'La Toubib:sell' },
+  { label: 'Trier avec Jaeger pour vendre', value: 'Jaeger:sell' },
+  { label: 'Trier avec Peacekeeper pour vendre', value: 'Peacekeeper:sell' },
+  { label: 'Trier avec Ragman pour vendre', value: 'Ragman:sell' },
+  { label: 'Trier avec Fence pour vendre', value: 'Fence:sell' },
+  { label: 'Trier avec Skier pour vendre', value: 'Skier:sell' },
+  { label: 'Trier avec Prapor pour vendre', value: 'Prapor:sell' },
+  { label: 'Trier avec la Toubib pour acheter', value: 'La Toubib:buy' },
+  { label: 'Trier avec Jaeger pour acheter', value: 'Jaeger:buy' },
+  { label: 'Trier avec Peacekeeper pour acheter', value: 'Peacekeeper:buy' },
+  { label: 'Trier avec Ragman pour acheter', value: 'Ragman:buy' },
+  { label: 'Trier avec Fence pour acheter', value: 'Fence:buy' },
+  { label: 'Trier avec Skier pour acheter', value: 'Skier:buy' },
+  { label: 'Trier avec Prapor pour acheter', value: 'Prapor:buy' },
 ]
-
 </script>
 
 <template>
@@ -135,9 +159,22 @@ const sortOptions = [
               </transition>
             </template>
           </n-select>
-          <n-grid v-if="data" v-scroll="onScroll" x-gap="12" y-gap="12" cols="5 xs:1 s:2 m:2 l:3 xl:4 2xl:5"
-            responsive="screen" class="overflow-y-auto h-[100vh] pr-2">
-            <n-gi v-for="item in data?.slice(0, pageIndex)" :key="item.id">
+          <n-back-top
+            :listen-to="target"
+            :bottom="220"
+            :visibility-height="10"
+            :style="{
+              transition: 'all .3s cubic-bezier(.4, 0, .2, 1)',
+            }"
+          />
+          <n-grid
+            v-if="dataToUse" ref=""
+            v-scroll="onScroll"
+            class="overflow-y-scroll h-[100vh] pr-2"
+            x-gap="12" y-gap="12" cols="5 xs:1 s:2 m:2 l:3 xl:4 2xl:5"
+            responsive="screen"
+          >
+            <n-gi v-for="item in dataToUse?.slice(0, pageIndex)" :key="item.id">
               <VItemCard :loading="loading" :item="item" />
             </n-gi>
           </n-grid>
